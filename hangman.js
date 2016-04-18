c9774cd7e2253e87b19ee3f82b3712001c243f04
jbain1 <jbainschool@gmail.com>
2016-04-18 23:15:54 +0000

    I'm having trouble finishing up this hangman program. I can't seem to set the win condition. Sample output after win condition is achieved: 'prompt: guess:  e e is the right letter [ 't', 'r', 'a', 'm', 'p', 'o', 'l', 'i', 'n', 'e' ] prompt: guess:  is the right letter [ '', 'r', 'a', 'm', 'p', 'o', 'l', 'i', 'n', 'e', '' ]'

---
+++ hangman.js
@@ -0,0 +1,66 @@
+var randomWordList = ["snakes", "trampoline", "poodle", "ganja", "toulouse", "fruitfly"];
+var prompt = require('prompt')
+
+function hangManDisplay(parts) {
+    console.log("|====-=====|")
+    switch (parts) {
+    case 6: console.log("............\n............\n............");break;
+    case 5: console.log(".....o......\n............\n............");break;
+    case 4: console.log(".....o......\n..../.......\n............");break;
+    case 3: console.log(".....o......\n..../|......\n............");break;
+    case 2: console.log(".....o......\n..../|\\.....\n............");break;
+    case 1: console.log(".....o......\n..../|\\.....\n..../.......");break;
+    case 0: console.log(".....o......\n..../|\\.....\n..../.\\.....");break;
+    }
+}
+
+var createSpaces = function (size){
+    var result = [ ];
+    for (var count=0;count < size;count++) {
+        result.push("_");
+    }
+    return result;
+};
+
+var answer = randomWordList[Math.floor(Math.random() * 5)]
+var splitAnswer = answer.split("");
+var spaces = createSpaces(answer.length);
+
+function hangMan(chances) {
+    prompt.get(['guess'], function (err, result) {
+      var yourGuess = result.guess;
+
+      
+      if (answer.indexOf(yourGuess) >= 0){
+          console.log(yourGuess + " is the right letter");
+          spaces[answer.indexOf(yourGuess)] = yourGuess;
+          spaces[answer.lastIndexOf(yourGuess)] = yourGuess;
+          console.log(spaces);
+      }
+      else {
+          chances -= 1;
+          console.log(spaces);
+          hangManDisplay(chances);
+        }
+        
+      if (spaces === splitAnswer) {
+            console.log("You win!")
+      }
+      
+      else {
+            hangMan(chances);
+      }
+      
+      if (chances === 0) {
+        console.log("You have died!");
+        console.log("The answer was " + answer + "!");
+        throw("Goodbye");
+      }
+});
+}
+
+hangMan(6);
+
+
+
+//full hangman: console.log(".....o......\n..../|\\.....\n..../.\\.....");break;
